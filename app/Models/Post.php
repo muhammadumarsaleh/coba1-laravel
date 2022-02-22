8cecc2fbe4e2b9503a2c9325e9f0d4503db3c1a3
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-02-22 09:29:47 +0800

    belajar model, collection dan controller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,5 +5,39 @@ namespace App\Models;
 
 class Post 
 {
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Umar Saleh",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis laborum deserunt inventore in molestiae quasi,
+            deleniti et omnis at animi? Pariatur reprehenderit facilis praesentium quo fugiat deleniti cumque magni consequuntur laborum.
+            Aperiam, minima, ipsum quam nisi dolore doloremque, iure aliquid ipsa iste dolor autem facere porro a tempore cupiditate
+            eligendi atque sit! Ab esse, praesentium repudiandae aspernatur voluptatibus dolores dolore nisi beatae eius quidem cupiditate
+            sunt voluptates quis accusamus id illum quod dolorem totam hic corrupti minima autem aperiam? Esse."
+        ],
+    
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Balqis",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio assumenda nihil itaque dicta? Laudantium dolorem,
+            optio ratione consequatur nisi eos illum asperiores provident a ad cupiditate, porro, id totam eveniet? Accusantium, fugit
+            eaque eveniet nesciunt incidunt error cumque deserunt dolore expedita sint velit illum qui repudiandae dicta ea fuga optio
+            blanditiis aliquid quasi facilis exercitationem voluptatibus inventore natus iusto. Magnam beatae blanditiis et sunt provident
+            labore, recusandae accusantium consequatur nemo quibusdam nobis itaque voluptate libero, neque voluptatem tenetur quaerat.
+            Hic ut assumenda laboriosam sit! Quo aspernatur autem dicta neque sint illum maxime aliquid doloribus, alias laudantium blanditiis, deleniti facere possimus."
+        ]
+    ];
 
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        return $posts->firstWhere('slug', $slug);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/home', function () {
@@ -32,78 +37,5 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Muhammad Umar Saleh",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis laborum deserunt inventore in molestiae quasi,
-            deleniti et omnis at animi? Pariatur reprehenderit facilis praesentium quo fugiat deleniti cumque magni consequuntur laborum.
-            Aperiam, minima, ipsum quam nisi dolore doloremque, iure aliquid ipsa iste dolor autem facere porro a tempore cupiditate
-            eligendi atque sit! Ab esse, praesentium repudiandae aspernatur voluptatibus dolores dolore nisi beatae eius quidem cupiditate
-            sunt voluptates quis accusamus id illum quod dolorem totam hic corrupti minima autem aperiam? Esse."
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Balqis",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio assumenda nihil itaque dicta? Laudantium dolorem,
-            optio ratione consequatur nisi eos illum asperiores provident a ad cupiditate, porro, id totam eveniet? Accusantium, fugit
-            eaque eveniet nesciunt incidunt error cumque deserunt dolore expedita sint velit illum qui repudiandae dicta ea fuga optio
-            blanditiis aliquid quasi facilis exercitationem voluptatibus inventore natus iusto. Magnam beatae blanditiis et sunt provident
-            labore, recusandae accusantium consequatur nemo quibusdam nobis itaque voluptate libero, neque voluptatem tenetur quaerat.
-            Hic ut assumenda laboriosam sit! Quo aspernatur autem dicta neque sint illum maxime aliquid doloribus, alias laudantium blanditiis, deleniti facere possimus."
-        ]
-    ];
-
-    
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-// halaman single post
-Route::get('posts/{slug}', function($slug) {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Muhammad Umar Saleh",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis laborum deserunt inventore in molestiae quasi,
-            deleniti et omnis at animi? Pariatur reprehenderit facilis praesentium quo fugiat deleniti cumque magni consequuntur laborum.
-            Aperiam, minima, ipsum quam nisi dolore doloremque, iure aliquid ipsa iste dolor autem facere porro a tempore cupiditate
-            eligendi atque sit! Ab esse, praesentium repudiandae aspernatur voluptatibus dolores dolore nisi beatae eius quidem cupiditate
-            sunt voluptates quis accusamus id illum quod dolorem totam hic corrupti minima autem aperiam? Esse."
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Balqis",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio assumenda nihil itaque dicta? Laudantium dolorem,
-            optio ratione consequatur nisi eos illum asperiores provident a ad cupiditate, porro, id totam eveniet? Accusantium, fugit
-            eaque eveniet nesciunt incidunt error cumque deserunt dolore expedita sint velit illum qui repudiandae dicta ea fuga optio
-            blanditiis aliquid quasi facilis exercitationem voluptatibus inventore natus iusto. Magnam beatae blanditiis et sunt provident
-            labore, recusandae accusantium consequatur nemo quibusdam nobis itaque voluptate libero, neque voluptatem tenetur quaerat.
-            Hic ut assumenda laboriosam sit! Quo aspernatur autem dicta neque sint illum maxime aliquid doloribus, alias laudantium blanditiis, deleniti facere possimus."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
