a39ea9669b1ee40fa9eafa502b79f5cad8b3e411
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-03-01 08:19:04 +0800

    belajar database, migration, eluquent, dan belajar post model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,13 +2,14 @@
 
 @section('container')
 
+<h1>Halaman Blog Posts</h1>
+
 @foreach($posts as $post)
   <article class="mb-5">
     <h2>
-        <a href="/posts/{{ $post["slug"] }} ">{{ $post["title"] }}</a>
+        <a href="/posts/{{ $post->slug }} ">{{ $post->title }}</a>
     </h2>
-    <h5>By: {{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <p>{{ $post->excerpt }}</p>
   </article>
 @endforeach
 
