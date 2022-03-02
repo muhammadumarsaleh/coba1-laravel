ceaa96624aa73ef6da9e85f146a244db2b9eea93
muhammadumarsaleh <92623313+muhammadumarsaleh@users.noreply.github.com>
2022-03-02 13:28:56 +0800

    belajar post category dan eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
 @section('container')
 
         <h1 class="mb-5">{{ $post->title }}</h1>
+        <p>By. Muhammad Umar Saleh in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
 
     <a href="/blog">Back to Posts</a>
-@endsection
+@endsection 
